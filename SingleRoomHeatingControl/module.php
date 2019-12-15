803d9c8e112ff98f5a43e7b6198550efea070be6
zerfasm <56298551+zerfasm@users.noreply.github.com>
2019-12-15 13:30:15 +0100

    Update module.php

--- SingleRoomHeatingControl/module.php
+++ SingleRoomHeatingControl/module.php
@@ -10,7 +10,8 @@
 			$this->RegisterPropertyString('RoomName', "");
 			$this->RegisterPropertyInteger('ActTempID', 0);
 			$this->RegisterPropertyInteger('SetTempID', 0);
-
+			$this->RegisterPropertyString('AbsentSetTemp', "19");
+			$this->RegisterPropertyString('WindowSetTemp', "6");
 			
 			// Time Schedule
 			$this->RegisterPropertyInteger('WeeklyTimeTableEventID', 0);
