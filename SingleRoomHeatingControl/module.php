d46e5597598c65c374e569cc9f57d2efae7b1500
zerfasm <56298551+zerfasm@users.noreply.github.com>
2019-12-15 13:57:45 +0100

    Update module.php

--- SingleRoomHeatingControl/module.php
+++ SingleRoomHeatingControl/module.php
@@ -1,5 +1,8 @@
 <?php
+	require_once __DIR__.'/../libs/traits.php';  // Allgemeine Funktionen
 	class SingleRoomHeatingControl extends IPSModule {
+		
+		use ProfileHelper, DebugHelper;
 
 		public function Create()
 		{
@@ -10,8 +13,8 @@
 			$this->RegisterPropertyString('RoomName', "");
 			$this->RegisterPropertyInteger('ActTempID', 0);
 			$this->RegisterPropertyInteger('SetTempID', 0);
-			  $this->RegisterPropertyBoolean('AbsentSetTemp', true);
-			$this->RegisterPropertyBoolean('WindowSetTemp', true);
+			$this->RegisterPropertyBoolean('CreateAbsentSetTemp', true);
+			$this->RegisterPropertyBoolean('CreateWindowSetTemp', true);
 			
 			// Time Schedule
 			$this->RegisterPropertyInteger('WeeklyTimeTableEventID', 0);
@@ -35,6 +38,12 @@
 		{
 			//Never delete this line!
 			parent::ApplyChanges();
+			
+			// Create Absent Set Temperature
+        		$create = $this->ReadPropertyBoolean('CreateAbsentSetTemp');
+			
+			// Create Window Open Set Temperature
+        		$create = $this->ReadPropertyBoolean('CreateWindowSetTemp');
 		}
 
 	}
