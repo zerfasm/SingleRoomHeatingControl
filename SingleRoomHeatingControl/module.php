8905a60e3c144d7087459ad5174aed23bf028207
zerfasm <56298551+zerfasm@users.noreply.github.com>
2019-12-15 19:09:37 +0100

    Update module.php

--- SingleRoomHeatingControl/module.php
+++ SingleRoomHeatingControl/module.php
@@ -83,7 +83,10 @@
 			//Letzte Sollwert schreiben
 			$update = $this->SetValue('LastSetTemp', $SetTemp);
 			 
+			 If ($HeizProg == 0)
+			 {
 			 HM_WriteValueFloat(52525, 'MANU_MODE',$AbsenkTemp);
+			 }
 			
 		}
 
