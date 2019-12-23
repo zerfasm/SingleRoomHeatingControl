aad430f24f9cd305a0e007a68eb11f619228ffc4
zerfasm <56298551+zerfasm@users.noreply.github.com>
2019-12-23 19:18:12 +0100

    Update module.php

--- SingleRoomHeatingControl/module.php
+++ SingleRoomHeatingControl/module.php
@@ -82,7 +82,7 @@ class SingleRoomHeatingControl extends IPSModule
         	
 		// Anlegen der Daten für den Wochenplan
         	for ($i = 0; $i <= 6; $i++) {
-            	IPS_SetEventScheduleGroup($this->GetIDForIdent("Wochenplan_".$this->InstanceID), $i, pow(127, $i));
+            	IPS_SetEventScheduleGroup($this->GetIDForIdent("Wochenplan_".$this->InstanceID), $i, pow(0, $i));
         	}
 		IPS_SetEventScheduleAction($this->GetIDForIdent("Wochenplan_".$this->InstanceID), 1, "Absenken", 0x000FF, "SRHC_AbsenkTemp(\$_IPS['TARGET']");  
         	IPS_SetEventScheduleAction($this->GetIDForIdent("Wochenplan_".$this->InstanceID), 2, "Grundwärme", 0xFF9900, "SRHC_GrundTemp(\$_IPS['TARGET']");  
