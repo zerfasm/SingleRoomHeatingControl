a9524aaf5be0b22d1a5536bb54a20a9cb3d92f34
zerfasm <56298551+zerfasm@users.noreply.github.com>
2019-12-25 17:52:13 +0100

    Update module.php

--- SingleRoomHeatingControl/module.php
+++ SingleRoomHeatingControl/module.php
@@ -76,15 +76,17 @@ class SingleRoomHeatingControl extends IPSModule
 		// ID Instanz
 		$Instance = $this->InstanceID;
 		
-		// Trigger Fenster erstellen
+		// Trigger Fenster
 		If ($this->ReadPropertyInteger('WindowID') > 0)
 		{
 			$this->RegisterTriggerWindow("Fenster", "TriggerFenster", 0, $Instance, 0,"SRHC_Update(\$_IPS['TARGET']);");
 		};
 		
-		// Trigger Anwesenheit erstellen
+		// Trigger Anwesenheit
 		$this->RegisterTriggerPresence("Anwesenheit", "TriggerAnwesenheit", 0, $Instance, 0,"SRHC_Update(\$_IPS['TARGET']);");
 		
+		// Trigger Modus
+		$this->RegisterTriggerModus("Modus", "TriggerModus", 0, $Instance, 0,"SRHC_Update(\$_IPS['TARGET']);");
 	}
 	
 		public function AbsenkTemp()
@@ -316,6 +318,28 @@ class SingleRoomHeatingControl extends IPSModule
 			IPS_SetEventActive($EventID, true);  
 		}
 	}
-
+	
+	private function RegisterTriggerModus($Name, $Ident, $Typ, $Parent, $Position, $Skript)
+	{
+		$eid = @$this->GetIDForIdent($Ident);
+		if($eid === false) {
+			$eid = 0;
+		} elseif(IPS_GetEvent($eid)['EventType'] <> $Typ) {
+			IPS_DeleteEvent($eid);
+			$eid = 0;
+		}
+		
+		//we need to create one
+		if ($eid == 0) {
+		    $EventID = IPS_CreateEvent($Typ);
+			IPS_SetEventTrigger($EventID, 1, $this->ReadPropertyInteger('ModID'));
+			IPS_SetParent($EventID, $Parent);
+			IPS_SetIdent($EventID, $Ident);
+			IPS_SetName($EventID, $Name);
+			IPS_SetPosition($EventID, $Position);
+			IPS_SetEventScript($EventID, $Skript); 
+			IPS_SetEventActive($EventID, true);  
+		}
+	}
 }
 ?>
