ac833c99766edcd2598d31bb507db827820da289
zerfasm <56298551+zerfasm@users.noreply.github.com>
2019-12-26 10:35:37 +0100

    Update module.php

--- SingleRoomHeatingControl/module.php
+++ SingleRoomHeatingControl/module.php
@@ -86,7 +86,7 @@ class SingleRoomHeatingControl extends IPSModule
 		$this->RegisterTriggerPresence("Anwesenheit", "TriggerAnwesenheit", 0, $Instance, 0,"SRHC_Update(\$_IPS['TARGET']);");
 		
 		// Trigger Modus
-		$this->RegisterTriggerMod("Programm", "TriggerProgramm", 0, $Instance, 0,"SRHC_Update(\$_IPS['TARGET']);");
+		$this->RegisterTriggerProgram("Programm", "TriggerProgramm", 0, $Instance, 0,"SRHC_Update(\$_IPS['TARGET']);");
 	}
 	
 	public function AbsenkTemp()
