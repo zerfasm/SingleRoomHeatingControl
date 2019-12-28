b0b9f45e13c30df3274d513aca3147c8f66ec9b5
zerfasm <56298551+zerfasm@users.noreply.github.com>
2019-12-28 10:45:06 +0100

    Update module.php

--- SingleRoomHeatingControl/module.php
+++ SingleRoomHeatingControl/module.php
@@ -39,10 +39,6 @@ class SingleRoomHeatingControl extends IPSModule
 		
 		// Steuerungsmodus
 		$this->RegisterPropertyInteger('SteuerMod', 0);
-		
-		$this->RegisterPropertyString('Absenken', ""); 
-		$this->RegisterPropertyString('Grundwärme', ""); 
-		$this->RegisterPropertyString('heizen', ""); 
 	}
 	
 	public function Destroy()
@@ -91,20 +87,6 @@ class SingleRoomHeatingControl extends IPSModule
 		
 		// Trigger Steuerungsmodus
 		$this->RegisterTriggerSteuerMod("Steuerungsmodus", "TriggerSteuerMod", 0, $Instance, 0,"SRHC_Update(\$_IPS['TARGET']);");
-		
-				
-		// Wochenplan Normal
-		$this->RegisterEventNormal("Wochenplan Normal", "EventWochenplan_Normal", 2, $Instance, 8);
-		
-		IPS_SetEventScheduleAction($this->GetIDForIdent("EventWochenplan_Normal"), 1, "Absenken", 0x000FF, "SRHC_AbsenkTemp(\$_IPS['TARGET']");  
-        	IPS_SetEventScheduleAction($this->GetIDForIdent("EventWochenplan_Normal"), 2, "Grundwärme", 0xFF9900 , "SRHC_GrundTemp(\$_IPS['TARGET']");  
-        	IPS_SetEventScheduleAction($this->GetIDForIdent("EventWochenplan_Normal"), 3, "Heizen", 0xFF0000, "SRHC_HeizTemp(\$_IPS['TARGET']");
-		
-		// Anlegen der Daten für den Wochenplan Normal
-        	IPS_SetEventScheduleGroup($this->GetIDForIdent("EventWochenplan_Normal"), 0, 31); //Mo - Fr (1 + 2 + 4 + 8 + 16)
-		IPS_SetEventScheduleGroup($this->GetIDForIdent("EventWochenplan_Normal"), 1, 96); //Sa + So (32 + 64)
-		
-		
 	}
 	
 	public function AbsenkTemp()
@@ -358,27 +340,6 @@ class SingleRoomHeatingControl extends IPSModule
 			IPS_SetName($EventID, $Name);
 			IPS_SetPosition($EventID, $Position);
 			IPS_SetEventScript($EventID, $Skript); 
-			IPS_SetEventActive($EventID, true);  
-		}
-	}
-	
-	private function RegisterEventNormal($Name, $Ident, $Typ, $Parent, $Position)
-	{
-		$eid = @$this->GetIDForIdent($Ident);
-		if($eid === false) {
-			$eid = 0;
-		} elseif(IPS_GetEvent($eid)['EventType'] <> $Typ) {
-			IPS_DeleteEvent($eid);
-			$eid = 0;
-		}
-		
-		//we need to create one
-		if ($eid == 0) {
-		    $EventID = IPS_CreateEvent($Typ);
-			IPS_SetParent($EventID, $Parent);
-			IPS_SetIdent($EventID, $Ident);
-			IPS_SetName($EventID, $Name);
-			IPS_SetPosition($EventID, $Position);
 			IPS_SetEventActive($EventID, true);  
 		}
 	}
